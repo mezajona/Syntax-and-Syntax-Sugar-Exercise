92921d3b4784812351a1a6993b8331d9dfec00d7
Jonathan Meza <jonathansyear92@gmail.com>
2025-01-16 15:30:29 -0800

    Syntax and Syntax Sugar Exercise

--- SyntaxAndSyntaxSugarExercise/Program.cs
+++ SyntaxAndSyntaxSugarExercise/Program.cs
@@ -4,7 +4,18 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var answer = 9;
+            var response = answer < 9 ? $"{answer} is less than nine" : $"{answer} is greater than or equal to nine";
+            Console.WriteLine(response);
+            // if (answer < 9) 
+            // {
+            //     response = answer + " is less than nine";
+            // }
+            // else
+            // {
+            //     response = answer + " is greater than or equal to nine";
+            // }
+
         }
     }
 }
